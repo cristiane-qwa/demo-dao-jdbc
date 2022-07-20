554cd64b4bbb52deb694a842516de75f3ecee4da
cristiane-qwa <cristiane.andrade@qwasolucoes.com.br>
2022-07-20 09:23:37 -0300

    DaoFactory

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,5 +1,7 @@
 package apllication;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.model.entities.Department;
 import model.model.entities.Seller;
 
@@ -11,6 +13,9 @@ public class Program {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        
         System.out.println(seller);
     }
 
