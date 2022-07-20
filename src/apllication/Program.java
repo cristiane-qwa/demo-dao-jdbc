309a4c72a979b6d5540f985e8fedbe831aae5cba
cristiane-qwa <cristiane.andrade@qwasolucoes.com.br>
2022-07-20 09:08:48 -0300

    Seller entity class

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,12 +1,17 @@
 package apllication;
 
 import model.model.entities.Department;
+import model.model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        
+
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 
 }
